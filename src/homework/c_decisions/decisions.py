9ff25a0b04d058444217c2a4c0b5d63b1b4c5507
brianrotichacc <157081499+brianrotichacc@users.noreply.github.com>
2024-02-07 19:26:55 +0000

    homework_2_submission

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,1 +1,19 @@
-#
+# In the decisions.py folder, create function named get_options_ratio that accepts two parameters 
+# with names option, total and returns the ratio.
+def get_options_ratio(option, total):
+    ratio = option/total
+    return ratio
+
+def get_faculty_rating(ratio):
+    if 0.9 <= ratio < 1:
+        return "Excellent"
+    elif ratio > 0.8:
+        return "Very Good"
+    elif ratio > 0.7:
+        return "Good"
+    elif ratio > 0.6:
+        return "Needs Improvement"
+    else: #covers the case of 0 to 0.59
+        return "Unacceptable"
+    
+
--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,10 @@
-#
+import decisions
+# initialize variables by allowing users to input values from the keyboard and convert it to an integer value
+option = int(input("Please enter option value: "))
+total = int(input("Please enter total value: "))
+# Get the ratio by calling get options ratio function from decision file and assign it to a variable
+ratio = decisions.get_options_ratio(option, total)
+# get the rating by passing in the assigned ratio value from ratio function and pass it to faculty rating function
+rating = decisions.get_faculty_rating(ratio)
+# Display the result from the function
+print(rating)
