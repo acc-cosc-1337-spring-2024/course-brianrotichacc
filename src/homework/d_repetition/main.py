4e6a37db60813f7f7d9d2948914b6c21b86e0373
brianrotichacc <157081499+brianrotichacc@users.noreply.github.com>
2024-02-16 01:37:00 +0000

    update work on hw3

--- src/homework/d_repetition/main.py
+++ src/homework/d_repetition/main.py
@@ -1,1 +1,34 @@
-#
+import repetition
+
+homework_3_menu = int(input("Please enter your option: "))
+
+if homework_3_menu == 3:
+    global keep_going 
+    keep_going= input("Do you want to continue? ")
+    if keep_going == 'Y' or keep_going == 'y':
+        homework_3_menu = int(input("Please enter your option: "))
+    else:
+        exit()
+
+while keep_going=="Y" or keep_going=="y":
+    if homework_3_menu == 1:
+        num = int(input("Please enter a number: "))
+        if num>=0 and num<=10:
+            result = repetition.get_factorial(num)
+            print(f"The factorial of {num} is {result}")
+        else:
+            num = int(input("Please enter a valid number: "))
+            result = repetition.get_factorial(num)
+            print(f"The factorial of {num} is {result}")
+        # keep_going = input("Do you want to Continue? ")
+    elif homework_3_menu ==2:
+        num = int(input("Please enter a number: "))
+        if num>=0 and num<=100:
+            result = repetition.get_factorial(num)
+            print(f"The factorial of {num} is {result}")
+        else:
+            num = int(input("Please enter a valid number: "))
+            result = repetition.get_factorial(num)
+            print(f"The sum of odd numbers of {num} is {result}")
+    else:
+        print('Invalid Number Entry')
--- src/homework/d_repetition/repetition.py
+++ src/homework/d_repetition/repetition.py
@@ -1,1 +1,19 @@
-#
+def get_factorial(num):
+    # Initializing variable
+    factorial = 1
+    # For range loop to calculate factorial
+    for i in range(1, num + 1):
+        factorial *= i 
+    return factorial
+
+def sum_odd_numbers(num):
+    # Initialize sum_total variable
+    sum_total = 0
+    i=1
+    while i<=num:
+        #Check if the number is odd
+        if i%2 != 0:
+            #Add odd number to sum_total
+            sum_total += i
+        i+=1       
+    return sum_total
