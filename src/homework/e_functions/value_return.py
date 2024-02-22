519ec9dc7bdf6b2fd0a05000abc42471d7356a9e
brianrotichacc <157081499+brianrotichacc@users.noreply.github.com>
2024-02-22 23:04:36 +0000

    update windows clock

--- src/homework/e_functions/value_return.py
+++ src/homework/e_functions/value_return.py
@@ -1,1 +1,14 @@
-#
+
+def get_hour(sec):
+    hours_since_1970 = sec//3600
+    current_hour = hours_since_1970 % 24
+    return current_hour
+
+def get_minutes(sec):
+    minutes_since_1970 = sec//60
+    current_minute = minutes_since_1970 % 60
+    return current_minute
+
+def get_seconds(sec):
+    current_second = sec % 60
+    return current_second
