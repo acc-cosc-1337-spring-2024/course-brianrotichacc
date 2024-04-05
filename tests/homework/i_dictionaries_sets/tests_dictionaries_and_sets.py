a6c83b15c80e346cb0c5a1804a6a9dc7eb3e564e
brianrotichacc <157081499+brianrotichacc@users.noreply.github.com>
2024-04-05 19:14:53 +0000

    Update

--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -1,7 +1,7 @@
 import unittest
 from src.homework.i_dictionaries_sets.dictionary import add_inventory
 from src.homework.i_dictionaries_sets.dictionary import remove_inventory_widget,inventory
-
+from src.homework.i_dictionaries_and_sets.sets import get_students_who_took_prog1_and_prog2#,get_students_who_took_prog2_or_prog1,get_students_who_took_prog1_not_prog_2,get_students_who_took_prog2_not_prog_1
 
 class Test_Config(unittest.TestCase):
 
@@ -17,3 +17,6 @@ class Test_Config(unittest.TestCase):
     def test_remove_inventory_widget(self):
         remove_inventory_widget('widget1')
         self.assertEqual(len(inventory), 1)
+
+    def test_get_students_who_took_prog1_and_prog2(self):
+        self
