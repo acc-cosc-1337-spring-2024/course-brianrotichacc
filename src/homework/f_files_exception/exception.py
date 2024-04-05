a851999a195ea927e40e0ad726d873dfa6eed74b
brianrotichacc <157081499+brianrotichacc@users.noreply.github.com>
2024-04-05 01:39:46 +0000

    Exceptions

--- src/homework/f_files_exception/exception.py
+++ src/homework/f_files_exception/exception.py
@@ -1,1 +1,10 @@
-#
+num1 = input("Enter number 1: ")
+# num2 = input("Enter number 2: ")
+
+try:
+    if(num1.isDigit()):
+        print(num1)
+except Exception as e:
+    error_message = str(e)
+
+print(error_message)
